6572e9b17db6d7c7bfbb2d2de0c873ebed3c267a
Makana Edwards <makana@clublisi.com>
2026-07-01 17:57:00 +0000

    Add custom bash aliases for development environment

---
+++ .devcontainer/.bash_aliases.sh
@@ -0,0 +1,13 @@
+# .devcontainer/.bash_aliases
+
+dj() {
+    case "$1" in
+        make) shift; poetry run python manage.py makemigrations "$@" ;;
+        mig) shift; poetry run python manage.py migrate "$@" ;;
+        run) shift; poetry run python manage.py runserver "$@" ;;
+        super) shift; poetry run python manage.py createsuperuser "$@" ;;
+        *) poetry run python manage.py "$@" ;;
+    esac
+}
+
+alias ll="ls -la"
