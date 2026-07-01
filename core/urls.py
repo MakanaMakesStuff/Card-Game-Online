3bafe74ad62944a4c4623386dc4156bc3b3dd953
Makana Edwards <makana@clublisi.com>
2026-07-01 12:57:55 -0400

    Add pyproject.toml for project configuration and dependencies

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from wagtail import urls as wagtail_urls
+from wagtail.admin import urls as wagtailadmin_urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path('cms/', include(wagtailadmin_urls)),
+    path("", include(wagtail_urls))
+]
